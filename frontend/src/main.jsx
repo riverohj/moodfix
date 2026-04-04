54306bdc5d3389ae268c4b078b2cbef85b037c3e
Lourdes Miranda <lourdescoronela@gmail.com>
2026-04-04 17:13:12 +0000

    Quitado el background para que salte el de style.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./styles.css";
+import Favoritos from "./components/Favoritos";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Favoritos />
   </React.StrictMode>,
 );
 
